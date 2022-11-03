1b9ac855bdebc3f99a4d4588d523ce1db4c2cfbe
Yerzin Perez <yerzin24@hotmail.com>
2022-11-03 18:19:21 -0500

    Version 1 de web api

---
+++ src/app/modulos/estaciones/estaciones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EstacionesRoutingModule } from './estaciones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EstacionesRoutingModule
+  ]
+})
+export class EstacionesModule { }
---
+++ src/app/modulos/rutas/rutas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RutasRoutingModule } from './rutas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RutasRoutingModule
+  ]
+})
+export class RutasModule { }
